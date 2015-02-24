b45d08a94143ade095e4cba61f4bdbdf17466eef
Jeff Purcell <jeffrey.a.purcell@navy.mil>
2015-02-24 13:02:45 -0800

    Escaping dollar signs in nuspec template for pass-through tokens

--- nuget/content/template.nuspec.pp
+++ nuget/content/template.nuspec.pp
@@ -10,15 +10,15 @@
     <projectUrl>https://github.com/PSNS-IMF/[github project name here]</projectUrl>
     <licenseUrl>https://github.com/PSNS-IMF/[github project name here]/blob/master/LICENSE.md</licenseUrl>
     <releaseNotes>
-      ## 1.0.0.1
+      ## 1.0.0.0
       ### Features
       * Initial release
     </releaseNotes>
-    <version>$version$</version>
+    <version>$$version$$</version>
     <tags>PSNS</tags>
   </metadata>
   <files>
-    <file src="..\bin\$configuration$\$rootnamespace$.*" target="lib\net40\" />
-    <file src="..\bin\$configuration$\net45\$rootnamespace$.*" target="lib\net45\" />
+    <file src="..\bin\$$configuration$$\$rootnamespace$.*" target="lib\net40\" />
+    <file src="..\bin\$$configuration$$\net45\$rootnamespace$.*" target="lib\net45\" />
   </files>
 </package>
